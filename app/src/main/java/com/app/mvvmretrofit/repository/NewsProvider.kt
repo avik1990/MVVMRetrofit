03e0d9bf5f7f53fadf7662032bb2f6ae95ae59d3
Avik <avik1990@gmail.com>
2020-05-06 02:38:43 +0530

    File name Changed

--- app/src/main/java/com/app/mvvmretrofit/repository/NewsProvider.kt
+++ app/src/main/java/com/app/mvvmretrofit/repository/NewsProvider.kt
@@ -3,7 +3,7 @@ package com.app.mvvmretrofit.repository
 import com.app.mvvmretrofit.others.RetrofitClient
 import com.sankha.daggerdemo2.login.viewmodel.NewsLocalDataSource
 
-object DashboardProvider {
+object NewsProvider {
 
     fun getNewsRepository(): NewsRepository {
         return NewsRepository(
